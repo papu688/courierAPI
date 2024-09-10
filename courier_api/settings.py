c2d4daf50cfa4e681e7052665a9c62953fafd95f
papu_68 <giorgobianipaul@gmail.com>
2024-09-10 17:35:37 +0200

    add .env and env example files

--- courier_api/settings.py
+++ courier_api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_b8t8_f(=@yyd%m!d9hqd_yyewxnh8bwp%yj$1)wor41*q_s&_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,8 +85,12 @@ WSGI_APPLICATION = 'courier_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
--- deliveries/urls.py
+++ deliveries/urls.py
@@ -3,4 +3,11 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router
+router.register(r'users', CustomUserViewset)
+router.register(r'parcels', ParcelViewset)
+router.register(r'delivery_proofs', DeliveryProofViewset)
+
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
